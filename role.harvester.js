def28f92a318cd14d131f632492331ce3dce94bf
niarbeht <toddnandrew@gmail.com>
2016-06-26 02:10:32 -0500

    basic caching

--- role.harvester.js
+++ role.harvester.js
@@ -1,3 +1,4 @@
+var utilsRoom = require('utils.room');
 var roleHarvester = {
 
     /** @param {Creep} creep **/
@@ -11,13 +12,7 @@ var roleHarvester = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN) &&
-                                structure.energy < structure.energyCapacity;
-                    }
-            });
+            var targets = roomUtils.getSpawnOrExtension(creep.room);
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
--- utils.room.js
+++ utils.room.js
@@ -1,9 +1,34 @@
+var spawnsAndExtensions_LT = [];
+var spawnsAndExtensions = [];
 var utilsRoom = {
+    /**
+     * @param {Room} room The room to grab stuff for.
+     * @returns List of spawns or extensions in the room whose energy is not full
+     */
+    generateSpawnExtensionList: function(room) {
+        spawnsAndExtensions[room] = room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+            }
+        });
+    },
 
-    var lastTick = 0;
-    /** @param {Creep} creep **/
-    run: function(creep) {
+    /**
+     * @param room The room to grab stuff for.
+     * @returns List of spawns or extensions in the room whose energy is not full
+     */
+    getSpawnOrExtension: function(room) {
+        if (spawnsAndExtensions_LT[room] == Game.time) {
+            return spawnsAndExtensions[room];
+        }
+        else {
+            generateSpawnExtensionList(room);
+            spawnsAndExtensions_LT[room] = Game.time;
+            return spawnsAndExtensions[room];
+        }
     }
 };
 
-module.exports = roleBuilder;
+module.exports = utilsRoom;
