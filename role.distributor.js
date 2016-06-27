d38d5f2576965641ac3a4617941e09572f497dd7
niarbeht <toddnandrew@gmail.com>
2016-06-26 23:15:59 -0500

    Distributors and harvesters now load balance

--- role.distributor.js
+++ role.distributor.js
@@ -1,18 +1,18 @@
 var utilsRoom = require('utils.room');
+
 var roleDistributor = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        if("" == creep.memory.source) {
+            console.log("Performing distributor load balance.");
+            roleDistributor.loadBalance(creep.room);
+        }
         if(creep.carry.energy == 0) {
-            var sources = utilsRoom.getNonEmptyContainersAndStorages(creep.room);
-            
-            if(sources.length) {
-                if(sources[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
-                }
-            }
-            else {
-                //Get out the way!
+            var source = Game.getObjectById(creep.memory.source);
+
+            if(source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
             }
         }
         else {
@@ -21,6 +21,27 @@ var roleDistributor = {
             if(creep.transfer(sinks[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sinks[0]);
             }
+        }
+    },
+
+    loadBalance: function(room) {
+        var distributors = _.filter(Game.creeps, function (creep) { return creep.memory.role == "distributor" && creep.room == room; });
+        var sources = utilsRoom.getAllSources(room);
+        var sinks = [];
+
+        //Generate sinks mapping
+        for (var src in sources) {
+            sinks[sources[src]] = sources[src].pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (o) => {
+                    return (o instanceof StructureContainer ||
+                        o instanceof StructureStorage);
+                }
+            });
+        }
+
+        for(var i = 0; i < distributors.length; i++) {
+            var source = sources[i % sources.length];
+            distributors[i].memory.source = sinks[source].id;
         }
     }
 };
--- role.harvester.js
+++ role.harvester.js
@@ -1,12 +1,17 @@
 var utilsRoom = require('utils.room');
+
 var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        if(("" == creep.memory.source) || ("" == creep.memory.sink)) {
+            console.log("Performing harvester load balance.");
+            roleHarvester.loadBalance(creep.room);
+        }
         if(creep.carry.energy < creep.carryCapacity) {
             var src = Game.getObjectById(creep.memory.source);
             if(creep.harvest(src) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(src);
+                creep.moveTo(src); //reusePath 500 holds the path for a long time.
             }
             var sink = Game.getObjectById(creep.memory.sink);
 
@@ -18,6 +23,31 @@ var roleHarvester = {
             if(creep.transfer(sink, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sink);
             }
+        }
+    },
+
+    /**
+     * Load balances the harvesters in a room.
+     */
+    loadBalance: function(room) {
+        var harvesters = _.filter(Game.creeps, function (creep) { return creep.memory.role == "harvester" && creep.room == room; });
+        var sources = utilsRoom.getAllSources(room);
+        var sinks = [];
+
+        //Generate sinks mapping
+        for (var src in sources) {
+            sinks[sources[src]] = sources[src].pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (o) => {
+                    return (o instanceof StructureContainer ||
+                        o instanceof StructureStorage);
+                }
+            });
+        }
+
+        for(var i = 0; i < harvesters.length; i++) {
+            var source = sources[i % sources.length];
+            harvesters[i].memory.source = source.id;
+            harvesters[i].memory.sink = sinks[source].id;
         }
     }
 };
