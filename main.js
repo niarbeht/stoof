f1a6d8563a1e957a4170ec58b219e3db2fa198ba
niarbeht <toddnandrew@gmail.com>
2016-06-26 23:15:16 -0500

    Basic CPU accounting

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ var roleMaintainer = require('role.maintainer');
 var roleDistributor = require('role.distributor');
 var roleAttacker = require('role.attacker');
 var numHarvesters = 0;
-var desiredHarvesters = 10;
+var desiredHarvesters = 4;
 var numBuilders = 0;
-var desiredBuilders = 3;
+var desiredBuilders = 8;
 var numUpgraders = 0;
-var desiredUpgraders = 3;
-var numMaintainers = 0;
-var desiredMaintainers = 2;
+var desiredUpgraders = 4;
 var numDistributors = 0;
-var desiredDistributors = 2;
+var desiredDistributors = 4;
+var numMaintainers = 0;
+var desiredMaintainers = 4;
 var numDefenders = 0;
-var desiredDefenders = 9;
+var desiredDefenders = 16;
 
 module.exports.loop = function () {
 //TODO add round-robin GC
@@ -76,21 +76,24 @@ module.exports.loop = function () {
 
     if (Game.spawns.Origin.spawning == null) {
         if (numHarvesters < desiredHarvesters) {
-            var harvesterLoadout = [MOVE,MOVE,WORK,WORK,CARRY];
+            var harvesterLoadout = [MOVE,MOVE,MOVE,WORK,WORK,WORK,CARRY];
             if(Game.spawns.Origin.canCreateCreep(harvesterLoadout) == OK) {
-                var newName = Game.spawns.Origin.createCreep(harvesterLoadout, undefined, {role: 'harvester', source: '55db32ccefa8e3fe66e05429', sink: '577043ecb50323c754c7551c'}); //or 55db32ccefa8e3fe66e0542a
+                var newName = Game.spawns.Origin.createCreep(harvesterLoadout, undefined, {role: 'harvester', source: '', sink: ''}); //or 55db32ccefa8e3fe66e0542a 
                 switch(newName) {
                     case ERR_NOT_ENOUGH_ENERGY:
                         console.log('Insufficient energy to spawn harvester');
+                        break;
+                    case ERR_INVALID_ARGS:
+                        console.log('Invalid arguments on harvester creation.');
                         break;
                     default:
                         console.log('Spawning new harvester: ' + newName);
                 }
             }
         } else if (numDistributors < desiredDistributors) {
-            var distributorLoadout = [MOVE,MOVE,MOVE,CARRY,CARRY,CARRY];
+            var distributorLoadout = [MOVE,MOVE,MOVE,MOVE,MOVE,CARRY,CARRY,CARRY,CARRY,CARRY];
             if(Game.spawns.Origin.canCreateCreep(distributorLoadout) == OK) {
-                var newName = Game.spawns.Origin.createCreep(distributorLoadout, undefined, {role: 'distributor', harvestTarget: 0});
+                var newName = Game.spawns.Origin.createCreep(distributorLoadout, undefined, {role: 'distributor', source: ''});
                 switch(newName) {
                     case ERR_NOT_ENOUGH_ENERGY:
                         console.log('Insufficient energy to spawn distributor');
@@ -100,9 +103,9 @@ module.exports.loop = function () {
                 }
             }
         } else if (numUpgraders < desiredUpgraders) {
-            var upgraderLoadout = [MOVE,MOVE,WORK,WORK,CARRY];
+            var upgraderLoadout = [MOVE,MOVE,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY];
             if(Game.spawns.Origin.canCreateCreep(upgraderLoadout) == OK) {
-                var newName = Game.spawns.Origin.createCreep(upgraderLoadout, undefined, {role: 'upgrader', harvestTarget: 0});
+                var newName = Game.spawns.Origin.createCreep(upgraderLoadout, undefined, {role: 'upgrader'});
                 switch(newName) {
                     case ERR_NOT_ENOUGH_ENERGY:
                         console.log('Insufficient energy to spawn upgrader');
@@ -112,9 +115,9 @@ module.exports.loop = function () {
                 }
             }
         } else if (numBuilders < desiredBuilders) {
-            var builderLoadout = [MOVE,MOVE,WORK,WORK,CARRY];
+            var builderLoadout = [MOVE,MOVE,MOVE,MOVE,WORK,WORK,WORK,CARRY];
             if(Game.spawns.Origin.canCreateCreep(builderLoadout) == OK) {
-                var newName = Game.spawns.Origin.createCreep(builderLoadout, undefined, {role: 'builder', harvestTarget: 0});
+                var newName = Game.spawns.Origin.createCreep(builderLoadout, undefined, {role: 'builder'});
                 switch(newName) {
                     case ERR_NOT_ENOUGH_ENERGY:
                         console.log('Insufficient energy to spawn builder');
@@ -124,9 +127,9 @@ module.exports.loop = function () {
                 }
             }
         } else if (numMaintainers < desiredMaintainers) {
-            var maintainerLoadout = [MOVE,MOVE,WORK,WORK,CARRY];
+            var maintainerLoadout = [MOVE,MOVE,WORK,CARRY];
             if(Game.spawns.Origin.canCreateCreep(maintainerLoadout) == OK) {
-                var newName = Game.spawns.Origin.createCreep(maintainerLoadout, undefined, {role: 'maintainer', harvestTarget: 0});
+                var newName = Game.spawns.Origin.createCreep(maintainerLoadout, undefined, {role: 'maintainer'});
                 switch(newName) {
                     case ERR_NOT_ENOUGH_ENERGY:
                         console.log('Insufficient energy to spawn maintainer');
@@ -137,23 +140,25 @@ module.exports.loop = function () {
             }
         } else if (numDefenders < desiredDefenders) {
             //BUILD SOLDIERS
-            var defenderLoadout = [TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK];
+            var defenderLoadout = [TOUGH,TOUGH,MOVE,MOVE,MOVE,ATTACK,MOVE,ATTACK,MOVE,ATTACK];
             if(Game.spawns.Origin.canCreateCreep(defenderLoadout) == OK) {
                 var newName = Game.spawns.Origin.createCreep(defenderLoadout, undefined, {role: 'defender'});
                 switch(newName) {
                 case ERR_NOT_ENOUGH_ENERGY:
-                    console.log('Insufficient energy to spawn builder');
+                    console.log('Insufficient energy to spawn defender');
                     break;
                 default:
-                    console.log('Spawning new builder: ' + newName);
+                    console.log('Spawning new defender: ' + newName);
                 }
             }
         }
     }
+    
     numHarvesters = 0;
     numBuilders = 0;
     numUpgraders = 0;
     numMaintainers = 0;
     numDistributors = 0;
     numDefenders = 0;
+    Memory.cpuBuf[Game.time % 100] = Game.cpu.getUsed();
 }
